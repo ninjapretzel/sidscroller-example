b697403319a3a63ea99c37af4a73e49ab7aedafb
ninjapretzel <ninjapretzel@yahoo.com>
2020-04-04 21:37:50 -0600

    Adds comments and changes some variable names

--- Assets/Scripts/SidescrollController.cs
+++ Assets/Scripts/SidescrollController.cs
@@ -4,8 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 
-
-
+/// <summary> Reasonably polished movement code. </summary>
 public class SidescrollController : PixelPerfectBehavior {
 	#region Input Simplifier
 	public Dictionary<string, bool> keys;
@@ -36,38 +35,63 @@ public class SidescrollController : PixelPerfectBehavior {
 	public bool Held(string key) { return binds.ContainsKey(key) ? keys[key] : false; }
 	#endregion
 
+	/// <summary> Draw some debugging information </summary>
 	public bool DEBUG_DRAW = false;
 
 	[Header("Movement")]
-	public float walkSpeed = 6;
-	public float dashSpeed = 11;
+	/// <summary> Units per second walking movement. 1 unit = 16 pixels = 1 tile </summary>
+	public float walkSpeed = 7;
+	/// <summary> Units per second dashing movement. 1 unit = 16 pixels = 1 tile </summary>
+	public float dashSpeed = 16;
+	/// <summary> Gravity acceleration per second. </summary>
 	public float gravity = 33f;
+	/// <summary> Terminal falling velocity. </summary>
 	public float terminalVelocity = 40;
+	/// <summary> Velocity applied when jumping </summary>
 	public float jumpPower = 15.0f;
+	/// <summary> Distance to snap to ground surface. Default = 1/16 </summary>
 	public float snapDistance = 0.0625f;
+	/// <summary> Distance to use to check for collision. Default = 2/16 </summary>
 	public float skinWidth = 0.125f;
-	
+	/// <summary> Falling rate when clinging to a wall </summary>
 	public float clingSpeed = 1f;
+	/// <summary> How quickly vertical speed changes when clinging </summary>
 	public float clingResponse = 11f;
-	public float groundedFriction = 90f;
+	/// <summary> Friction applied when grounded </summary>
+	public float groundedFriction = 255f;
+	/// <summary> Friction applied when in the air </summary>
 	public float friction = 30f;
+	/// <summary> Delay after physics is applied before control resumes</summary>
 	public float controlDelay = .3f;
 
-	public Vector2 kickPower = new Vector2(15, 15);
-	public Vector2 dodgePower = new Vector2(22, 5);
+	/// <summary> Velocity applied when wall kicking </summary>
+	public Vector2 kickPower = new Vector2(8, 13);
+	/// <summary> Velocity applied when dodging </summary>
+	public Vector2 dodgePower = new Vector2(17, 4);
 
+	/// <summary> Character sprite animator </summary>
 	[Header("Links")]
 	public SpriteAnimator spriteAnimator;
+	/// <summary> Effect sprite animator </summary>
 	public SpriteAnimator effectAnimator;
+	/// <summary> Physics collider </summary>
 	public Collider2D col;
 
+	/// <summary> Sprite X direction. 1.0 if right, -1.0 if left. </summary>
 	[Header("Animations")]
 	public float defaultXFacing = 1.0f;
+	/// <summary> Time the shoot pose is held after shooting </summary>
 	public float shootTimeout = .25f;
-	public float meleeComboTime = 1.1f;
+	/// <summary> Time between button presses where combos will connect </summary>
+	public float meleeComboTime = 0.8f;
+	/// <summary> Percentage melee animation must finish before another melee attack can begin </summary>
 	public float meleeComboPercent = .63f;
+	/// <summary> Character name, used as a prefix to look up animations for the character </summary>
 	public string animationPrefix = "Reimu";
+	/// <summary> Last played animation </summary>
 	public string currentAnimation = "";
+
+	#region animation "auto" properties
 	public SpriteAnim Climb { get { return LoadSpriteAnim(MemberName()); } }
 	public SpriteAnim ClimbDone { get { return LoadSpriteAnim(MemberName()); } }
 	public SpriteAnim Dash { get { return LoadSpriteAnim(MemberName()); } }
@@ -83,7 +107,20 @@ public class SidescrollController : PixelPerfectBehavior {
 	public SpriteAnim Moving { get { return LoadSpriteAnim(MemberName()); } }
 	public SpriteAnim Rising { get { return LoadSpriteAnim(MemberName()); } }
 	public SpriteAnim WallCling { get { return LoadSpriteAnim(MemberName()); } }
-	
+	/// <summary> Helpful macro that grabs the calling member name of anything that calls it. 
+	/// <para>Makes it easier to make properties utilizing the <see cref="data"/> field, eg </para> <para><code>
+	/// public <see cref="JsonObject"/> Attributes { get { return data.Get&lt;<see cref="JsonObject"/>&gt;(MemberName()); } }
+	/// </code></para></summary>
+	/// <param name="caller"> Autofilled by compiler </param>
+	/// <returns> Name of member calling this method. </returns>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static string MemberName([CallerMemberName] string caller = null) { return caller; }
+
+	#endregion
+
+	/// <summary> Loads an animation by name. </summary>
+	/// <param name="animName"> Animation name to load </param>
+	/// <returns> SpriteAnim loaded from a resource, or <see cref="defaultAnim"/> if none was loaded. </returns>
 	private SpriteAnim LoadSpriteAnim(string animName) {
 		if (lastShoot < shootTimeout) {
 			SpriteAnimAsset shootAsset = Resources.Load<SpriteAnimAsset>(animationPrefix + animName + "Shoot");
@@ -96,45 +133,58 @@ public class SidescrollController : PixelPerfectBehavior {
 		currentAnimation = animName;
 		return asset != null ? asset.data : defaultAnim;
 	}
+	/// <summary> Default animation to use if none was loaded. </summary>
 	public SpriteAnim defaultAnim;
-
-	/// <summary> Helpful macro that grabs the calling member name of anything that calls it. 
-	/// <para>Makes it easier to make properties utilizing the <see cref="data"/> field, eg </para> <para><code>
-	/// public <see cref="JsonObject"/> Attributes { get { return data.Get&lt;<see cref="JsonObject"/>&gt;(MemberName()); } }
-	/// </code></para></summary>
-	/// <param name="caller"> Autofilled by compiler </param>
-	/// <returns> Name of member calling this method. </returns>
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public static string MemberName([CallerMemberName] string caller = null) { return caller; }
-
 	// internal state
 	// internal lets other things in the same assembly access them
 	// but doesn't serialize them unless we tell it to.
+	/// <summary> Current velocity. </summary>
 	Vector3 velocity;
+	/// <summary> Current input vector. </summary>
 	Vector3 input;
+	/// <summary> Current facing direction. 1.0f if facing right, -1.0f if facing left. </summary>
 	float facing = 1.0f;
+	/// <summary> Current movement vector. </summary>
 	Vector3 movement;
+	/// <summary> Current applied movement vector. </summary>
 	Vector3 moved;
+	/// <summary> Is the character currently on the ground this frame? </summary>
 	bool isGrounded;
+	/// <summary> Did the character bump their head this frame? </summary>
 	bool bumpedHead;
+	/// <summary> Does the character have a wall on their left this frame? </summary>
 	bool wallOnLeft;
+	/// <summary> Does the character have a wall on their right this frame? </summary>
 	bool wallOnRight;
+	/// <summary> Is the character currently dashing? </summary>
 	bool dashing = false;
+	/// <summary> Const for melee input recieved state  </summary>
 	const int MELEE_STARTING = 0;
+	/// <summary> Const for melee animation playing  </summary>
 	const int MELEE_PLAYING = 1;
+	/// <summary> Const for not meleeing </summary>
 	const int MELEE_DONE = 2;
-	float lastShoot = 3;
+	/// <summary> Current melee state </summary>
+	int meleeState = MELEE_DONE;
+	/// <summary> Chosen melee pose </summary>
+	int meleePose = 1;
+	/// <summary> Timer since the last melee attack was used. </summary>
 	float lastMelee = 3;
+	/// <summary> Timer since the last shot was fired. </summary>
+	float lastShoot = 3;
+	/// <summary> Timer since the last dodge was used. </summary>
 	float lastDodge = 3;
+	/// <summary> Timer since the last wallkick was used. </summary>
 	float lastKick = 3;
 	
 
-	int meleeState = MELEE_DONE;
-	int meleePose = 1;
-
+	/// <summary> Can the character actually move right now? </summary>
 	bool canMove = true;
+	/// <summary> Is the character wall-clinging? </summary>
 	public bool clinging { get { return !isGrounded && (clingingLeft || clingingRight); } }
+	/// <summary> Is the character wall-clinging to the left? </summary>
 	public bool clingingLeft { get { return (input.x < 0 && wallOnLeft); } }
+	/// <summary> Is the character wall-clinging to the right? </summary>
 	public bool clingingRight { get { return (input.x > 0 && wallOnRight); } }
 
 	/// <summary> Preallocated array for collisions </summary>
@@ -157,15 +207,18 @@ public class SidescrollController : PixelPerfectBehavior {
 	/// <summary> See if we are touching a wall on the right </summary>
 	public bool CheckWallOnRight() { return IsTouching(Vector3.right * skinWidth); }
 	
+	/// <summary> Called by Unity on load. </summary>
 	void Awake() {
 		Application.targetFrameRate = 60;	
 	}
 	
+	/// <summary> Called by Unity before first frame. </summary>
 	void Start() {
 		UpdateAnimation(Vector3.zero, Vector3.zero);
 		if (spriteAnimator != null) { spriteAnimator.anim = Idle; }
 	}
 	
+	/// <summary> Called by Unity every frame. </summary>
 	void Update() {
 		ResetPixelPerfect();
 		UpdateKeys();
@@ -265,11 +318,14 @@ public class SidescrollController : PixelPerfectBehavior {
 		DebugDraw();
 	}
 
+	/// <summary> Called by Unity every frame, after every object has had its Update. </summary>
 	void LateUpdate() {
 		ApplyPixelPerfect();
 	}
 
+	/// <summary> Update the animation based on applied and requested movement. </summary>
 	private void UpdateAnimation(Vector3 moved, Vector3 movement) {
+		
 		if (spriteAnimator == null) { 
 			Transform check = transform.Find("Sprite");
 			if (check != null) { spriteAnimator = check.GetComponentInChildren<SpriteAnimator>();  }
@@ -318,6 +374,7 @@ public class SidescrollController : PixelPerfectBehavior {
 		}
 
 		if (isGrounded) {
+			lastDodge = 3;
 		} else {
 			spriteAnimator.anim = velocity.y > 0 ? Rising : Falling;
 
@@ -332,6 +389,7 @@ public class SidescrollController : PixelPerfectBehavior {
 
 	}
 
+	/// <summary> Match melee effect animation </summary>
 	private void UpdateEffectAnimation() {
 		if (effectAnimator == null) {
 			Transform check = transform.Find("Effect");
@@ -350,6 +408,7 @@ public class SidescrollController : PixelPerfectBehavior {
 		}
 	}
 
+	/// <summary> Draws some debug information in the editor. </summary>
 	private void DebugDraw() {
 		
 		if (DEBUG_DRAW) {
@@ -378,6 +437,9 @@ public class SidescrollController : PixelPerfectBehavior {
 
 
 
+	/// <summary> Attempt to move the character, and get how far they actually moved. </summary>
+	/// <param name="movement"> Requested movement </param>
+	/// <returns> Applied movement </returns>
 	private Vector3 Move(Vector3 movement) {
 		col = GetComponent<Collider2D>();
 		//Physics.BoxCastNonAlloc()
@@ -386,7 +448,9 @@ public class SidescrollController : PixelPerfectBehavior {
 		moved += DoMove(new Vector3(movement.x, 0, 0));
 		return moved;
 	}
-
+	/// <summary> Move the character until they are in contact with some surface. </summary>
+	/// <param name="movement"> Requested movement </param>
+	/// <returns> Applied movement </returns>
 	private Vector3 DoMoveUntilTouching(Vector3 movement) {
 		bool move = true;
 		float maxDist = movement.magnitude;
@@ -421,7 +485,9 @@ public class SidescrollController : PixelPerfectBehavior {
 
 		return Vector3.zero;
 	}
-
+	/// <summary> Attempt to move the character, and get how far they actually moved. `</summary>
+	/// <param name="movement"> Requested movement. </param>
+	/// <returns> Applied movement </returns>
 	private Vector3 DoMove(Vector3 movement) {
 		bool move = true;
 
@@ -450,7 +516,10 @@ public class SidescrollController : PixelPerfectBehavior {
 		return Vector3.zero;
 	}
 
-	public bool IsTouching(Vector3 swipe) {
+	/// <summary> Check if the character would touch something when their collider is swept along a direction </summary>
+	/// <param name="sweep"> Sweep direction/distance vector </param>
+	/// <returns> True if they would hit something, false otherwise. </returns>
+	public bool IsTouching(Vector3 sweep) {
 		if (col != null) {
 			if (col is BoxCollider2D) {
 				BoxCollider2D box = col as BoxCollider2D;
@@ -458,7 +527,7 @@ public class SidescrollController : PixelPerfectBehavior {
 				//float adjWidth = 1f;
 				
 
-				if (DEBUG_DRAW) {
+				if (DEBUG_DRAW) { // Draw the touching check.
 					void DrawBox(Vector3 p, Vector3 s, Color? color = null) {
 						Color c = color ?? Color.white;
 						Vector3 c1 = s; 
@@ -472,16 +541,16 @@ public class SidescrollController : PixelPerfectBehavior {
 						Debug.DrawLine(p + c1, p + c4, c);
 					}
 					DrawBox(point, box.size * .5f);
-					DrawBox(point+swipe, box.size * .5f, Color.cyan);
+					DrawBox(point+sweep, box.size * .5f, Color.cyan);
 				}
 
 				Vector2 adjSize = box.size;
 				//adjSize.x *= adjWidth;
 
-				int numCollisions = Physics2D.BoxCastNonAlloc(point, adjSize, 0, swipe, raycastHits, swipe.magnitude + snapDistance);
+				int numCollisions = Physics2D.BoxCastNonAlloc(point, adjSize, 0, sweep, raycastHits, sweep.magnitude + snapDistance);
 				if (numCollisions > 0) {
 					int lowest = -1;
-					float lowestDistance = swipe.magnitude + snapDistance;
+					float lowestDistance = sweep.magnitude + snapDistance;
 
 					for (int i = 0; i < numCollisions; i++) {
 						if (raycastHits[i].collider == col) { continue; } // Skip own collider.
@@ -496,16 +565,6 @@ public class SidescrollController : PixelPerfectBehavior {
 					}
 
 					if (lowest >= 0) {
-						//if (moveToTouch) {
-						//	Vector2 diff = (Vector2)transform.position - raycastHits[lowest].centroid;
-						//	Vector2 proj = Vector3.Project(diff, swipe);
-						//	float angle = Vector2.Angle(proj, swipe);
-							
-						//	if (angle <= .1f && proj.magnitude < swipe.magnitude + snapDistance) {
-						//		transform.position = raycastHits[lowest].centroid;
-						//	}
-						//}
-
 						return true;
 					}
 
